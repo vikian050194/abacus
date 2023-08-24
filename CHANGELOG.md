31ad9fd86f2f2336eb8f2e28566c7cbbf670b94f
Kirill Vinogradov <vikian050194@gmail.com>
2023-08-24 18:35:01 +0200

    MVP

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.1.0] - 2023-08-24
+
+### Added
+
+- `addition` task type
+- autofocus on input after page is loaded
+- score with right answers and total count of problems
+
+[Unreleased]: https://github.com/vikian050194/click/compare/v0.1.0...HEAD
+[0.2.0]: https://github.com/vikian050194/click/compare/v0.1.0...v0.2.0
+[0.1.0]: https://github.com/vikian050194/click/releases/tag/v0.1.0
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,11 @@
 import { Builder, convert, replace } from "fandom";
 
+const randomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 document.addEventListener("DOMContentLoaded", function () {
     const builder = new Builder();
 
@@ -7,9 +13,58 @@ document.addEventListener("DOMContentLoaded", function () {
 
     $root.classList.add("container");
 
-    builder.open("header").text("header").close();
-    builder.open("main").text("main").close();
-    builder.open("footer").text("footer").close();
+    const TASK = "task";
+    const SCORE = "score";
+
+    const min = 1;
+    const max = 99;
+
+    let expected = 0;
+    let actual = 0;
+    let score = 0;
+    let total = 0;
+
+    const updateTask = (description) => {
+        document.getElementById(TASK).innerText = description;
+    };
+
+    const updateScore = (score, total) => {
+        document.getElementById(SCORE).innerText = `${score}/${total}`;
+    };
+
+    const clearInput = () => {
+        const input = document.querySelector("input");
+        input.value = "";
+        input.focus();
+    };
+
+    const generate = () => {
+        const a = randomInt(min, max);
+        const b = randomInt(min, max);
+        expected = a + b;
+        updateTask(`${a}+${b}`);
+    };
+
+    const onChange = (e) => {
+        actual = parseInt(e.target.value);
+        score += expected === actual ? 1 : 0;
+        total++;
+        updateScore(score, total);
+        generate();
+        clearInput();
+    };
+
+    builder
+        .open("header").span().text("abacus").close().span({id: SCORE}).close().close()
+        .open("main")
+        .div({ id: TASK }).close()
+        .input({ type: "number", pattern: "[0-9]*", inputmode: "numeric" }).onChange(onChange)
+        .close()
+        .open("footer").text("footer").close();
 
     replace($root, convert(builder.done()));
+
+    generate();
+    updateScore(score, total);
+    clearInput();
 });
